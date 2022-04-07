f9cc1c0e01e18a8a1007d661a3b0124222140630
Antonio <amarquez1980@gmail.com>
2022-04-07 22:32:59 +0200

    chore(@tfm/tfm-build-tools): configure typescript

--- packages/tfm-build-tools/__tests__/tfm-build-tools.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const tfmBuildTools = require('..');
-
-describe('@tfm/tfm-build-tools', () => {
-    it('needs tests');
-});
--- packages/tfm-build-tools/lib/tfm-build-tools.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = tfmBuildTools;
-
-function tfmBuildTools() {
-    // TODO
-}
